47a4af867851a724fef8b4ea0462533fdb87eef9
KaraboZulu <ST10519843@rcconnect.edu.za>
2026-06-14 20:09:18 +0200

    add QuickChat menu with send message option

--- src/login/Main.java
+++ src/login/Main.java
@@ -90,7 +90,7 @@ public class Main {
                         System.out.println("Invalid option. Please try again.");
                 }
             }
-        }
+        } 
         scan.close();
     }
 }
